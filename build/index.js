fd05b65bc6c55af068ae39555da1bb1fb5924586
luanfsouza <luanferreira7777@gmail.com>
2023-12-17 16:48:41 -0300

    node_modules n\build/\npackage-lock.json\n yarn.lock

--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server/server");
-const portaPadrao = 3333;
-server_1.server.listen(process.env.PORT || portaPadrao, () => console.log(`rodando na porta ${process.env.PORT || portaPadrao}`));
